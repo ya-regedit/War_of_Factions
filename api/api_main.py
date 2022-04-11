0e995e1888c0a6d8b466a6dcc9441e1554285df6
ya-regedit <aat-3@yandex.ru>
2022-04-12 00:01:44 +0500

    Добавил procfile и requirements

--- api/api_main.py
+++ api/api_main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from data import db_session
@@ -12,7 +14,8 @@ api = Api(app)
 def main():
     db_session.global_init('db/artifacts.db')
     app.register_blueprint(arts_api.blueprint)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
