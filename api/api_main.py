05c07a90b8b65dcaf564d70f5006fba94171ff1c
ya-regedit <aat-3@yandex.ru>
2022-04-12 00:10:07 +0500

    Добавил procfile и requirements

--- api/api_main.py
+++ api/api_main.py
@@ -14,9 +14,9 @@ api = Api(app)
 def main():
     db_session.global_init('db/artifacts.db')
     app.register_blueprint(arts_api.blueprint)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
     main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
